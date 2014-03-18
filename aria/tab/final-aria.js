c6cb5f5a3b754577405b806c18e481ab979c1c68
David Sturley <david.sturley@deque.com>
2014-03-18 15:09:53 -0400

    Initial commit

---
+++ aria/tab/final-aria.js
@@ -0,0 +1,42 @@
+jQuery('.tabs').each(function (_, tab) {
+	var $tab = jQuery(tab),
+		$tabpanels = $tab.find('[role="tabpanel"]'),
+		$tabs = $tab.find('[role="tab"]');
+
+	$tabs.on('click', function (e) {
+		// grab the target of the event
+		var $target = jQuery(e.target);
+
+		// find the panel that is now selected
+		var panel = document.getElementById($target.attr('aria-controls'));
+
+		// remove selected from previously selected tabs
+		$tabs.attr({
+			'aria-selected': 'false',
+			tabindex: -1
+		});
+		$tabpanels.attr({
+			'aria-hidden': 'true',
+			'aria-expanded': 'false'
+		});
+
+
+		// update selected class to newly selected tabs
+		jQuery(panel).attr({
+			'aria-hidden': 'false',
+			'aria-expanded': 'true'
+		});
+		$target.attr({
+			'aria-selected': 'true',
+			tabindex: 0
+		});
+
+	});
+
+	$tab.on('keydown', function (e) {
+		var currentIndex = $tabs.index(e.target);
+		keyboardListener($tabs, currentIndex, e.which);
+		
+	});
+
+});
